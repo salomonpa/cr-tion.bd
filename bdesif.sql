eaf6bfcda32f1e440a02c5d79f21b029c90fafe9
Bemssé flora <fsislinebemssee@gmail.com>
2023-07-12 18:19:45 +0200

    mis ajour

--- bdesif.sql
+++ bdesif.sql
@@ -1,4 +1,3 @@
-create database bd_esiftechnologie;
 
 CREATE TABLE Clients(
     clientid Int primary key not null auto_increment,
@@ -15,21 +14,21 @@ values('Bemssé', 'sisline', 'pattedoie', 'ftoungai@gmail.com',56498921),
       ('Feidanga', 'larissa', 'ouaga2000','larios@gmail.com', 75513513),
       ('Touangai',' marie_ange', 'ouaga200', 'ange@gmail.com', 75539971),
       ( 'Kabore', 'antoine', 'cissin', 'kb@gmail.com',70202581),
-       ('Zongo', 'aboubacar', 'goughin', 'zbou@gmail.com', 6060333)
+       ('Zongo', 'aboubacar', 'goughin', 'zbou@gmail.com', 6060333);
  
 
  CREATE TABLE Categorie (
  Categorieid INT PRIMARY KEY NOT NULL,
-    Nom VARCHAR(50),  
+    Nom VARCHAR(50)
 );
 INSERT INTO Categorie (CategorieID, Nom)
 VALUES (1,'electromenager');
 
 CREATE TABLE articles (
-    articleid INT PRIMARY KEY AUTO INCREMENT NOT NULL,
+    articleid INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nom VARCHAR(200),
     quantite INT(10),
-    prix INT (10,),
+    prix INT (10),
     categoid int,
     FOREIGN KEY (categoid) REFERENCES Categorie (Categorieid)
 );
@@ -40,45 +39,39 @@ VALUES ('FOUR', 10, 55000 ),
        ('télé.plasma', 10, 80000 );
 
 
-
-CREATE TABLE Fournisseur (
-    fournisseurid INT PRIMARY KEY AUTO INCREMENT NOT NULL,
+       CREATE TABLE Fournisseur(
+    fournisseurid INT PRIMARY KEY AUTO_INCREMENT ,
     Nom VARCHAR(100),
     Contact INT(50),
     Email VARCHAR(50)
-
 );
-
-INSERT INTO Fournisseur(nom, contact, Email)
-VALUES ('job', 25253640, 'jobt@gmail.com')
-        (albert,70405563, 'alb@gmail.com')
-        (jonathan, 50553312, 'jt@gmail.com')
-        (jules, 74502020, 'jul@gmail.com')
-
-
+insert into Fournisseur(nom, contact, Email)
+VALUES ('job', 25253640, 'jobt@gmail.com'),
+        ('albert',70405563,'alb@gmail.com'),
+        ('jonathan', 50553312,'jt@gmail.com'),
+        ('jules', 74502020, 'jul@gmail.com');
 
 CREATE TABLE Achat (
-    achatid INT PRIMARY KEY AUTO_INCREMENT,NOT NULL
-    articleid INT
+    achatid INT PRIMARY KEY AUTO_INCREMENT,
+    articleid INT,
     fournisseurid INT,
-    Quantite (10),
+    Quantite int(10) ,
     DateAchat DATE,
     FOREIGN KEY (fournisseurid) REFERENCES Fournisseur(fournisseurid)
+    );
 
-
-); 
 
 INSERT INTO Achat (ArticleID, Quantite, DateAchat)
 VALUES (1, 10, '12-07-2023');
 
 
 CREATE TABLE Ventes (
-    VenteID INT PRIMARY KEY, NOT NULL
+    VenteID INT PRIMARY KEY NOT NULL,
     articleid INT,
     Quantite INT(5),
     DateVente DATE,
     clientid int,
-    FOREIGN KEY (clientid) REFERENCES Clients(clientid)
+    FOREIGN KEY (clientid) REFERENCES Clients(clientid),
     FOREIGN KEY(articleid) REFERENCES articles(articleid)
 ); 
 
