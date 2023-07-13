20349fddbabfd6548bdfb7467937a50e41456560
Bemssé flora <fsislinebemssee@gmail.com>
2023-07-13 16:12:11 +0200

    correction

--- bdesif.sql
+++ bdesif.sql
@@ -1,15 +1,14 @@
-
-CREATE TABLE Clients(
+CREATE TABLE clients(
     clientid Int primary key not null auto_increment,
     nom VARCHAR(200),
     prenom VARCHAR(200),
     adresse VARCHAR(200),
     email VARCHAR(200),
-    téléphone Int(25)
+    telephone Int(25)
 );
 
 
-insert into Clients (nom, prenom, adresse, email,  téléphone)
+insert into clients (nom, prenom, adresse, email,  telephone)
 values('Bemssé', 'sisline', 'pattedoie', 'ftoungai@gmail.com',56498921),
       ('Feidanga', 'larissa', 'ouaga2000','larios@gmail.com', 75513513),
       ('Touangai',' marie_ange', 'ouaga200', 'ange@gmail.com', 75539971),
@@ -17,66 +16,75 @@ values('Bemssé', 'sisline', 'pattedoie', 'ftoungai@gmail.com',56498921),
        ('Zongo', 'aboubacar', 'goughin', 'zbou@gmail.com', 6060333);
  
 
- CREATE TABLE Categorie (
- Categorieid INT PRIMARY KEY NOT NULL,
+ CREATE TABLE categories (
+ categoriesid INT PRIMARY KEY NOT NULL,
     Nom VARCHAR(50)
 );
-INSERT INTO Categorie (CategorieID, Nom)
-VALUES (1,'electromenager');
+INSERT INTO categories (categoriesid, nom)
+VALUES (1,'electromenager'),
+        (2,'ordinateur'),
+        (3,'télévision'),
+        (4, 'téléphone'),
+        (5,'ventilateur');
 
 CREATE TABLE articles (
     articleid INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nom VARCHAR(200),
-    quantite INT(10),
-    prix INT (10),
-    categoid int,
-    FOREIGN KEY (categoid) REFERENCES Categorie (Categorieid)
+    quantite INT,
+    prix_achat INT,
+    prix_vente INT,
+    categoriesid INT,
+    FOREIGN KEY (categoriesid) REFERENCES categories (categoriesid)
 );
 
-INSERT INTO articles ( nom, quantite, prix )
-VALUES ('FOUR', 10, 55000 ),
-       ('congélateur', 10, 10000 ),
-       ('télé.plasma', 10, 80000 );
+INSERT INTO articles ( nom, quantite, prix_achat, prix_vente )
+VALUES ('fOUR', 10, 55000, 95000),
+       ('congélateur', 10, 85000, 140000 ),
+       ('télé.plasma', 10, 80000, 130000 );
 
 
-       CREATE TABLE Fournisseur(
+       CREATE TABLE fournisseur(
     fournisseurid INT PRIMARY KEY AUTO_INCREMENT ,
-    Nom VARCHAR(100),
-    Contact INT(50),
-    Email VARCHAR(50)
+    nom VARCHAR(100),
+    contact INT,
+    email VARCHAR(50)
 );
-insert into Fournisseur(nom, contact, Email)
+INSERT INTO fournisseur(nom, contact, Email)
 VALUES ('job', 25253640, 'jobt@gmail.com'),
         ('albert',70405563,'alb@gmail.com'),
         ('jonathan', 50553312,'jt@gmail.com'),
         ('jules', 74502020, 'jul@gmail.com');
 
-CREATE TABLE Achat (
+CREATE TABLE achat (
     achatid INT PRIMARY KEY AUTO_INCREMENT,
     articleid INT,
     fournisseurid INT,
-    Quantite int(10) ,
-    DateAchat DATE,
-    FOREIGN KEY (fournisseurid) REFERENCES Fournisseur(fournisseurid)
+    prix_achat INT,
+    quantite Int ,
+    dateachat DATE,
+    FOREIGN KEY (fournisseurid) REFERENCES fournisseur(fournisseurid),
+    FOREIGN KEY (articleid) REFERENCES articles(articleid)
+  
     );
 
 
-INSERT INTO Achat (ArticleID, Quantite, DateAchat)
-VALUES (1, 10, '12-07-2023');
+INSERT INTO achat (articleid, prix_achat, quantite, fournisseurid, dateachat)
+VALUES (1, 80000, 10, 1, '2023-07-17');
 
 
-CREATE TABLE Ventes (
-    VenteID INT PRIMARY KEY NOT NULL,
+CREATE TABLE ventes (
+    venteid INT PRIMARY KEY NOT NULL,
     articleid INT,
-    Quantite INT(5),
-    DateVente DATE,
-    clientid int,
-    FOREIGN KEY (clientid) REFERENCES Clients(clientid),
+    quantite INT,
+    prix_vente INT,
+    datevente DATE,
+    clientid INT,
+    FOREIGN KEY (clientid) REFERENCES clients(clientid),
     FOREIGN KEY(articleid) REFERENCES articles(articleid)
 ); 
 
-INSERT INTO Ventes (VenteID, ArticleID, Quantite, DateVente, clientid)
-VALUES (1, 1, 5, '11-07-2023', 1);
+INSERT INTO ventes (venteid, articleid, quantite, prix_vente, datevente, clientid)
+VALUES (1, 1, 5, 15000, '2023-07-11', 1);
 
 
 
